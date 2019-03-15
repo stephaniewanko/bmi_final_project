9dc0b4a72ea0ddeb7ad5224078388643212cccf6
Stephanie (Mullane) Wankowicz <15824327+stephaniewanko@users.noreply.github.com>
2019-03-15 15:38:53 -0700

    Update test.py

--- testing/test.py
+++ testing/test.py
@@ -7,4 +7,10 @@ assert NN.sigmoid_der(2)==0.5
 
 #testing the autoencoder
 
-assert
+def test_autoencoder():
+    x = np.identity(8)
+    y = np.identity(8)
+    NN = Neural_Network(input_layer_size=8, hidden_layer_size=3, output_layer_size=8, Lambda=2e-6)
+    NN.train(x,y,10000,0.45)  
+    predict = NN.forward(x)
+    assert len(predict.shape[0]) == 8
