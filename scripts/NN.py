8dd1150955397715bd64075818c7c111e42e7e2f
Stephanie (Mullane) Wankowicz <15824327+stephaniewanko@users.noreply.github.com>
2019-03-15 15:34:29 -0700

    Add files via upload

--- scripts/NN.py
+++ scripts/NN.py
@@ -1,9 +1,7 @@
-#Stephanie Wankowicz
-#3/13/2019
+##on GIT
+
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy import optimize
-import matplotlib.pyplot as plt
 
 '''
  Implement a feed-forward, three-layer, neural network with standard sigmoidal units.
@@ -12,118 +10,78 @@ import matplotlib.pyplot as plt
  We expect that you will be able toproduce fast enough code to be of use in the learning task at hand.
  You will want to make surethat your code can learn the 8x3x8 encoder problem prior to attempting the Rap1learningtask.
 '''
+
 '''
-first, we are going to develop a class to represent the neural network
-'''
-class neuralNetwork(object):
-    def __init__(self,input_layer_size, hidden_layer_size, output_layer_size, Lambda): 
-   '''
-   NN has 3 layers(input, hidden, output). 
-   INPUT (hyperparameters): 
+   NN has 3 layers(input, hidden, output).
+   INPUT (hyperparameters):
    Input layer size- number of nodes for the input
    Hidden layer size- number of nodes in the hidden layer
    Output layer size- number of nodes in the output layer
    Lambda- Regularization Rate; preventing the weights from getting out of control. Pushes each weight and the mean of the weights towards zero.
-   '''
+'''
+
+class Neural_Network(object):
+    def __init__(self,input_layer_size, hidden_layer_size, output_layer_size, Lambda):
         self.inputLayerSize = input_layer_size #setting the input layer size
         self.hiddenLayerSize = hidden_layer_size #setting the hidden layer size
         self.outputLayerSize = output_layer_size #setting the output layer size
-        # Initialize the sinapses with random weights
+        # Initialize with random weights
         self.W1 = np.random.randn(self.inputLayerSize, self.hiddenLayerSize) #setting the initial weights as random
         self.W2 = np.random.randn(self.hiddenLayerSize, self.outputLayerSize) #setting the initial weights as random (This is also what is helping to set up our NN to be 8x3x8).
         self.Lambda = Lambda #setting the lambda value as your input lambda
+    def forward(self, input):
+     '''
+     This function is going to move you forward through the NN.
+     '''
+     self.hidden = np.dot(input, self.W1) #take the dot product of the input and the first set of weights.
+     #print(self.hidden.shape)
+     self.hid_act = self.sigmoid(self.hidden) #Perform the activation function on the middle (hidden) layer.
+     #print(self.hid_act.shape)
+     self.output = np.dot(self.hid_act, self.W2) #Dot produce of the activated values in the middle (hidden) layer to produce the input for the third layer
+     #print(self.output.shape)
+     self.pred_out = self.sigmoid(self.output) #Perform the activation function on the third layer and produce the predicted output
+     return self.pred_out #return the third layer
 
-    def forwardPropagate(self, input): 
-     '''
-     This function is going to move you forward through the NN. 
-     '''
-        self.z2 = np.dot(input, self.W1) #It is going to take the dot product of the input and the first set of weights.
-        self.a2 = self.sigmoid(self.z2) #Perform the activation function on the middle (hidden) layer.
-        self.z3 = np.dot(self.a2, self.W2) #Dot produce of the activated values in the middle (hidden) layer to produce the input for the third layer
-        output = self.sigmoid(self.z3) #Perform the activation function on the third layer and produce the predicted output
-        return output #return the third layer 
+    def back_prop(self, input,output,learning_rate):
+        """
+		Optimize prediction by minimizing error with gradient descent
+		INPUT: expected output and output activation
+		"""
+        error=(output-self.pred_out)#calculating the error between the output and the predicted output layer
+        #calculate the derivative of the activation function to get the gradient slope
+        grad_out=self.sigmoid_der(self.pred_out)
+        grad_hid=self.sigmoid_der(self.hid_act)
+        delta_out=grad_out*error
+        #print(delta_out.shape)
+        delta_hid=np.dot(delta_out,self.W2.T) * grad_hid
+        #update weights
+        self.W2 += np.dot(self.hid_act.T,delta_out) * learning_rate
+        self.W1 += np.dot(input.T, delta_hid) * learning_rate
+        #create new weights=gradient * learning rate
+        return error
+
+    def train(self, input, known_output, iterations, learning_rate):
+        #print('starting!')
+        #i=0
+        error_list=[]
+        for i in range(iterations):
+            self.forward(input)
+            error=self.back_prop(input,known_output,learning_rate)
+            average_error=np.average(error)
+            i+=1
+        return average_error
 
     def sigmoid(self, z):
-    '''
-    Function to perform sigmoid activation function; doing this on an array.
-    This is a special case of the logistic function & will squash the output to be between 0 and 1.
-    INPUT: array of weighted values
-    OUTPUT: value between 0 and 1
-    '''
-        return 1 / (1 + np.exp(-z)) 
+        '''
+        Function to perform sigmoid activation function; doing this on an array.
+        This is a special case of the logistic function & will squash the output to be between 0 and 1.
+        INPUT: array of weighted values
+        OUTPUT: value between 0 and 1
+        '''
+        return 1 / (1 + np.exp(-z))
 
     def sigmoid_der(self, z):
-    '''
-    Function to perform the derivative of the sigmoid activation function; doing this on an array
-    '''
-        return np.exp(-z)/((1 + np.exp(-z)) ** 2) 
-
-    def cost(self, input, known_output): 
-     '''
-    Measurement of goodness of fit of NN output data to the known output. 
-    This will be used for our backpropagation.
-    It based on the average of the output activation values.
-    We also have our lambda value in here to regulaize 
-    sum the squared difference between the predicted and known output
-    ??lambda section
-     '''
-        self.h = self.forwardPropagate(input) #going to move forward through the neural network and get out some output value
-        J = 0.5 * np.sum((known_output - self.h) ** 2) / input.shape[0] + (self.Lambda/2)*(np.sum(self.W1**2)+np.sum(self.W2**2)) #calculate the difference beween output of forward NN & known output
-        return J
-
-    def cost_der(self, X, y):
-     '''
-     
-     '''
-        self.h = self.forwardPropagate(X)
-        delta3 = np.multiply(-(y - self.h), self.sigmoid_der(self.z3))
-        dJdW2 = np.dot(self.a2.T, delta3)/X.shape[0] + self.Lambda * self.W2
-        delta2 = np.dot(delta3, self.W2.T) * self.sigmoid_der(self.z2)
-        dJdW1 = np.dot(X.T, delta2)/X.shape[0] + self.Lambda * self.W1
-
-        return dJdW1, dJdW2
-
-    def unrollweights(self):
-        return np.concatenate((self.W1.ravel(), self.W2.ravel()))
-
-    def set_weights(self, weights):
-        W1_end = self.hiddenLayerSize * self.inputLayerSize
-        self.W1 = np.reshape(weights[0:W1_end], (self.inputLayerSize, self.hiddenLayerSize))
-        W2_end = W1_end + self.hiddenLayerSize * self.outputLayerSize
-        self.W2 = np.reshape(weights[W1_end:W2_end], (self.hiddenLayerSize, self.outputLayerSize))
-
-    def unrollGradients(self, X, y):
-        dJdW1, dJdW2 = self.cost_der(X, y)
-        return np.concatenate((dJdW1.ravel(), dJdW2.ravel()))
-
-# Class for train a neural network
-class Trainer(object):
-    def __init__(self, NN):
-        self.N = NN
-
-    def callback(self, weights):
-        self.N.set_weights(weights)
-        self.J.append(self.N.cost(self.X, self.y))
-
-    def costFunctionWrapper(self, weights, X, y):
-        self.N.set_weights(weights)
-        #print('here2')
-        cost = self.N.cost(X, y)
-        #print('here3')
-        grad = self.N.unrollGradients(X, y)
-        #print('here4')
-        #print(cost)
-        #print(grad)
-        return cost, grad
-
-    def train(self, X, y):
-        self.X = X
-        self.y = y
-        self.J = []
-        weights_0 = self.N.unrollweights() #initial weights
-        options = {'maxiter': 5000, 'disp': True}
-        #print('here')
-        opt = optimize.minimize(self.costFunctionWrapper, weights_0, jac=True, method='BFGS', args=(X, y), options=options, callback=self.callback)
-        #print(opt)
-        self.N.set_weithts(opt.x)
-        self.optimizationResults = opt
+        '''
+        Function to perform the derivative of the sigmoid activation function; doing this on an array
+        '''
+        return np.exp(-z)/((1 + np.exp(-z)) ** 2)
---
+++ scripts/main.py
@@ -0,0 +1,208 @@
+#RUN FUNCTIONS
+
+#import packages
+import numpy as np
+import pandas as pd
+import sklearn
+from Bio import SeqIO
+import re
+from NN import Neural_Network
+import matplotlib.pyplot as plt
+
+np.random.seed(0) #just getting the same random numbers so we can evaluate our NN
+
+
+#################__________________PART 1: Run autoencoder__________________###############
+#create identity 8x8 matrix
+x = np.identity(8)
+y = np.identity(8)
+
+NN = Neural_Network(input_layer_size=8, hidden_layer_size=3, output_layer_size=8, Lambda=2e-6)
+#tested multiple lambda values. This lambda value gave me the best
+#print(NN)
+#T=trainer(NN)
+#T.train(x, y)
+#print(T)
+#predict = NN.forward(x)
+#NN.train(x,y,10000,0.45)
+#T.train(x, y)
+#print(d1)
+#predict = NN.forward(x)
+
+print('predict')
+#rint(predict)
+print('Now rounding Matrix')
+# the original output is continous, but we want the output to be discrete
+#print(predict.round())
+
+
+
+
+#################__________________PART 2: DNA INPUT__________________###############
+
+#Pre-process DNA for NN input
+
+#1) remove some of the false negatives from the fasta file & create new fasta file.
+
+###########________________________________________#################
+pos_seq=pd.read_csv('rap1-lieb-positives.txt', sep='\t', header=None)
+neg_seq=list(SeqIO.parse("/Users/stephaniewankowicz/Dropbox/BMI_203/Final_Project/yeast-upstream-1k-negative.fa", 'fasta'))
+print(len(neg_seq))
+remove_seq=[]
+for y in range(len(neg_seq)):
+    #find and remove exact matches
+    for x in pos_seq[0]:
+        if re.search(x, str(neg_seq[y].seq)):
+            remove_seq.append(str(neg_seq[y].seq))
+            #print('found')
+        else:
+            continue
+remove_seq=set(remove_seq)
+
+with open('New_Neg_seq.fa', "w") as f:
+    for y in range(len(neg_seq)):
+        if str(neg_seq[y].seq) in remove_seq:
+            #print('no')
+            continue
+        else:
+            SeqIO.write(neg_seq[y], f, "fasta")
+
+def select_sequences(neg_seq_file, pos_seq_file, num_train):
+    #2) Create inputs from positive and negative sequences
+    #import positive
+    pos_seq=[]
+    pos_file= open(pos_seq_file, 'r')
+    for line in pos_file.readlines():
+        pos_seq.append(line.strip())
+    pos_input = list(np.random.choice(pos_seq, num_train))
+    #take random (consequetive) 17 bases of the neg seq (same size as positive seq)
+    neg_seq = []
+    neg_file = open('New_Neg_seq.fa', 'r')
+    for line in neg_file.readlines():
+        if (line[0] == '>'):
+            continue
+        else:
+            neg_seq.append(line.strip()[0:17])
+            #for each run through, only take 137 negative sequenes==# of positive sequences
+
+            #remove neg_seq with n
+    neg_input = list(np.random.choice(neg_seq, num_train))
+
+    #create negative and positive outputs
+    #one=positive
+    #zero=negative
+    pos_outputs = np.ones(len(pos_input)) #one
+    neg_outputs = np.zeros(len(neg_input)) #zeros
+
+    #combine and randomize input and outputs
+    inputs=np.append(pos_input, neg_input) #np.random.shuffle()
+    outputs=np.append(pos_outputs, neg_outputs)
+    combined = list(zip(inputs, outputs))
+    np.random.shuffle(combined)
+    inputs[:], outputs[:] = zip(*combined)
+    #for cross-validation, we are also going to select the testing postive and negative sequences
+    pos_test=np.setdiff1d(pos_seq,pos_input)
+    neg_test=np.setdiff1d(neg_seq,neg_input)
+    pos_output_test = np.ones(len(pos_test)) #one
+    neg_output_test = np.zeros(len(neg_test)) #zeros
+    input_test=np.append(pos_test, neg_test)
+    output_test=np.append(pos_output_test, neg_output_test)
+
+    return inputs, outputs, input_test, output_test
+
+
+#change DNA single letters into 4 letter matrix code
+###########________________________________________#################
+#A=0001
+#C=1000
+#G=0100
+#T=0010
+
+def DNA_input(inputs):
+    s = (len(inputs), 4*len(inputs[0])) #create matrix w/num of sequences by 4*17(DNA input length)
+    input_DNA = np.empty(s)
+    #print(input_DNA)
+    for i in range(len(inputs)):
+        for j in range(len(inputs[i])):
+            index = j * 4
+            if inputs[i][j] == "A":
+                input_DNA[i,index]=0
+                input_DNA[i,index+1]=0
+                input_DNA[i,index+2]=0
+                input_DNA[i,index+3]=1
+            elif inputs[i][j] == "C":
+                input_DNA[i,index]=1
+                input_DNA[i,index+1]=0
+                input_DNA[i,index+2]=0
+                input_DNA[i,index+3]=0
+            elif inputs[i][j] == "G":
+                input_DNA[i,index]=0
+                input_DNA[i,index+1]=1
+                input_DNA[i,index+2]=0
+                input_DNA[i,index+3]=0
+            elif inputs[i][j] == "T":
+                input_DNA[i,index]=0
+                input_DNA[i,index+1]=0
+                input_DNA[i,index+2]=1
+                input_DNA[i,index+3]=0
+    return input_DNA
+
+
+#################__________________PART 3: Train NN on DNA Sequences__________________###############
+#Train NN on positives and negatives (note: avoid overfitting)
+#print(outputs.shape)
+#print(input_DNA.shape)
+input_train, output_train, input_test_seq, output_test=select_sequences('/Users/stephaniewankowicz/Dropbox/BMI_203/Final_Project/New_Neg_seq.fa','rap1-lieb-positives.txt', 90)
+#print(output_train.shape)
+#print(output_train.shape[0])
+#print(output_train)
+input_DNA_train=DNA_input(input_train)
+#print(input_DNA_train)
+print(input_DNA_train.shape[0])
+print('starting DNA Neural Network')
+NN_DNA = Neural_Network(input_layer_size=68, hidden_layer_size=3,output_layer_size=180, Lambda=2e-6)  # initialize NN #input_layer_size=input_DNA.shape[0], output_layer_size=outputs.shape[0], hidden_layer_size=10, Lambda=0.00000005
+NN_DNA.train(input_DNA_train, output_train, iterations=50,learning_rate=0.45)
+#initial prediction
+print('training done')
+predict = NN_DNA.forward(input_DNA_train)
+#print(predict)
+#print(predict.round()) #want this continous for the actual output
+print('yay! something ran!!')
+
+def learn_parms():
+    error_list=pd.DataFrame()
+    for i in [3,5,10,20,30,40,50,60,100]: #number of hidden nodes to test
+        print(i)
+        for learning_rate in [0.01,0.1,0.25,0.5,0.7,1,5]:
+            #=k_folds
+            input_train, output_train, input_test_seq, output_test=select_sequences('/Users/stephaniewankowicz/Dropbox/BMI_203/Final_Project/New_Neg_seq.fa','rap1-lieb-positives.txt', 90)
+            input_DNA_train=DNA_input(input_train)
+            NN_learn = Neural_Network(input_layer_size=68, hidden_layer_size=i,output_layer_size=180, Lambda=2e-6)  # initialize NN #input_layer_size=input_DNA.shape[0], output_layer_size=outputs.shape[0], hidden_layer_size=10, Lambda=0.00000005
+            error=NN_learn.train(input_DNA_train, output_train, iterations=500,learning_rate=learning_rate)
+            #print(error)
+            #error_list.append(error)
+            error_list.loc[i,learning_rate]=error
+    print(error_list)
+    return error_list
+learning_errors=learn_parms()
+learning_errors.to_csv('learn_parms.csv')
+
+
+####______________________PART 4: CROSS VALIDATION______________________############
+def cross_validation(pos_seq_file, neg_seq_file,num_train_runs, num_samples_training):
+    """
+    pick a subset of data for training, leave rest for testing
+    We are going to select 180 sequences (90-positive, 90-negative) for training, leaving 74 (37-positive, 37-negative) for testing
+    INPUT: Positive and Negative Sequences
+    OUTPUT:
+    We are going to look at the learning rate and the number of nodesin the hidden layer
+    """
+    for i in range(1,50,5): # we are going to train X times
+        print(i)
+        for lr in [0.01,0.5,0.05]:
+            #=k_folds
+            input_train, output_train, input_test_seq, output_test=select_sequences('/Users/stephaniewankowicz/Dropbox/BMI_203/Final_Project/New_Neg_seq.fa','rap1-lieb-positives.txt', 90)
+        input_DNA_train=DNA_input(input_train)
+        #avg_accuracy, avg_auc, tprs, fprs = k_folds(train_seq, train_exp, learn_rate, num_hidden)
+        #output
+    return x_training, y_training, x_validation, y_validation
