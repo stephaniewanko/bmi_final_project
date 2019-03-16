26c43a02b7aeaddf632a32d024c45f01d86c84e3
Stephanie (Mullane) Wankowicz <15824327+stephaniewanko@users.noreply.github.com>
2019-03-15 22:41:13 -0700

    Add files via upload

--- scripts/main.py
+++ scripts/main.py
@@ -7,9 +7,9 @@ import sklearn
 from Bio import SeqIO
 import re
 from NN import Neural_Network
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
-np.random.seed(0) #just getting the same random numbers so we can evaluate our NN
+np.random.seed(10) #just getting the same random numbers so we can evaluate our NN
 
 
 #################__________________PART 1: Run autoencoder__________________###############
@@ -24,16 +24,15 @@ NN = Neural_Network(input_layer_size=8, hidden_layer_size=3, output_layer_size=8
 #T.train(x, y)
 #print(T)
 #predict = NN.forward(x)
-#NN.train(x,y,10000,0.45)
+NN.train(x,y,10000,0.6)
 #T.train(x, y)
 #print(d1)
-#predict = NN.forward(x)
-
+predict = NN.forward(x)
 print('predict')
 #rint(predict)
 print('Now rounding Matrix')
 # the original output is continous, but we want the output to be discrete
-#print(predict.round())
+print(predict.round())
 
 
 
@@ -45,8 +44,8 @@ print('Now rounding Matrix')
 #1) remove some of the false negatives from the fasta file & create new fasta file.
 
 ###########________________________________________#################
-pos_seq=pd.read_csv('data/rap1-lieb-positives.txt', sep='\t', header=None)
-neg_seq=list(SeqIO.parse("data/yeast-upstream-1k-negative.fa", 'fasta'))
+pos_seq=pd.read_csv('rap1-lieb-positives.txt', sep='\t', header=None)
+neg_seq=list(SeqIO.parse("/Users/stephaniewankowicz/Dropbox/BMI_203/Final_Project/yeast-upstream-1k-negative.fa", 'fasta'))
 print(len(neg_seq))
 remove_seq=[]
 for y in range(len(neg_seq)):
@@ -152,8 +151,8 @@ def DNA_input(inputs):
 #Train NN on positives and negatives (note: avoid overfitting)
 #print(outputs.shape)
 #print(input_DNA.shape)
-input_train, output_train, input_test_seq, output_test=select_sequences('data/New_Neg_seq.fa','data/rap1-lieb-positives.txt', 90)
-#print(output_train.shape)
+input_train, output_train, input_test_seq, output_test=select_sequences('/Users/stephaniewankowicz/Dropbox/BMI_203/Final_Project/New_Neg_seq.fa','rap1-lieb-positives.txt', 90)
+print(input_test_seq)
 #print(output_train.shape[0])
 #print(output_train)
 input_DNA_train=DNA_input(input_train)
@@ -161,7 +160,7 @@ input_DNA_train=DNA_input(input_train)
 print(input_DNA_train.shape[0])
 print('starting DNA Neural Network')
 NN_DNA = Neural_Network(input_layer_size=68, hidden_layer_size=3,output_layer_size=180, Lambda=2e-6)  # initialize NN #input_layer_size=input_DNA.shape[0], output_layer_size=outputs.shape[0], hidden_layer_size=10, Lambda=0.00000005
-NN_DNA.train(input_DNA_train, output_train, iterations=50,learning_rate=0.45)
+NN_DNA.train(input_DNA_train, output_train, iterations=500,learning_rate=0.45)
 #initial prediction
 print('training done')
 predict = NN_DNA.forward(input_DNA_train)
@@ -169,40 +168,80 @@ predict = NN_DNA.forward(input_DNA_train)
 #print(predict.round()) #want this continous for the actual output
 print('yay! something ran!!')
 
-def learn_parms():
-    error_list=pd.DataFrame()
-    for i in [3,5,10,20,30,40,50,60,100]: #number of hidden nodes to test
-        print(i)
-        for learning_rate in [0.01,0.1,0.25,0.5,0.7,1,5]:
-            #=k_folds
-            input_train, output_train, input_test_seq, output_test=select_sequences('data/New_Neg_seq.fa','data/rap1-lieb-positives.txt', 90)
-            input_DNA_train=DNA_input(input_train)
-            NN_learn = Neural_Network(input_layer_size=68, hidden_layer_size=i,output_layer_size=180, Lambda=2e-6)  # initialize NN #input_layer_size=input_DNA.shape[0], output_layer_size=outputs.shape[0], hidden_layer_size=10, Lambda=0.00000005
-            error=NN_learn.train(input_DNA_train, output_train, iterations=500,learning_rate=learning_rate)
-            #print(error)
-            #error_list.append(error)
-            error_list.loc[i,learning_rate]=error
-    print(error_list)
-    return error_list
-learning_errors=learn_parms()
-learning_errors.to_csv('learn_parms.csv')
-
 
 ####______________________PART 4: CROSS VALIDATION______________________############
-def cross_validation(pos_seq_file, neg_seq_file,num_train_runs, num_samples_training):
-    """
-    pick a subset of data for training, leave rest for testing
-    We are going to select 180 sequences (90-positive, 90-negative) for training, leaving 74 (37-positive, 37-negative) for testing
-    INPUT: Positive and Negative Sequences
-    OUTPUT:
-    We are going to look at the learning rate and the number of nodesin the hidden layer
-    """
-    for i in range(1,50,5): # we are going to train X times
-        print(i)
-        for lr in [0.01,0.5,0.05]:
-            #=k_folds
-            input_train, output_train, input_test_seq, output_test=select_sequences('data/New_Neg_seq.fa','data/rap1-lieb-positives.txt', 90)
+
+def learn_parms():
+    '''
+        This is our cross validation procedure that will also choose our hyperparmeters we are testing.
+        pick a subset of data for training, leave rest for testing
+        We are going to select 180 sequences (90-positive, 90-negative) for training, leaving 74 (37-positive, 37-negative) for testing
+        INPUT: Positive and Negative Sequences
+        OUTPUT: Error rate for every iteration, average error rate for each hyperparameter pair (number of hidden layer nodes, learning rate)
+        We are going to look at the learning rate and the number of nodesin the hidden layer
+    '''
+    error_pd=pd.DataFrame()
+    error_list=[]
+    count=1
+    for a in range(100): #we are choosing 100 different random cuts of our data to cross-validate
+        input_train, output_train, input_test, output_test=select_sequences('/Users/stephaniewankowicz/Dropbox/BMI_203/Final_Project/New_Neg_seq.fa','rap1-lieb-positives.txt', 90)
         input_DNA_train=DNA_input(input_train)
-        #avg_accuracy, avg_auc, tprs, fprs = k_folds(train_seq, train_exp, learn_rate, num_hidden)
-        #output
-    return x_training, y_training, x_validation, y_validation
+        input_DNA_test=DNA_input(input_test)
+        for i in [3,5,10,20,30,40,50,100]: #number of hidden nodes to test
+            print(i)
+            error_list=[]
+            for learning_rate in [0.01,0.1,0.25,0.5,0.7,1,5]:
+                NN_learn = Neural_Network(input_layer_size=68, hidden_layer_size=i,output_layer_size=180, Lambda=2e-6)  # initialize NN #input_layer_size=input_DNA.shape[0], output_layer_size=outputs.shape[0], hidden_layer_size=10, Lambda=0.00000005
+                error=NN_learn.train(input_DNA_train, output_train, iterations=500,learning_rate=learning_rate)
+                #test
+                NN_learn.forward(input_DNA_test)
+                print(error)
+                index_val=(count-2)
+                print(index_val)
+                error_list.append(error)
+                print(error_list)
+                #if count>2:
+                    #error_diff=error_list[index_val]-error
+                    #print(error_diff)
+                #print(error_list[1])
+                error_pd.loc[count,'learning_rate']=learning_rate
+                error_pd.loc[count,'hidden_nodes']=i
+                error_pd.loc[count,'error']=error
+                count+=1
+    #print(error_list)
+    return error_pd
+#learning_errors=learn_parms()
+#learning_errors.to_csv('learn_parms.csv')
+
+####______________________PART 5: Test______________________############
+#we are going to train the NN using the hyperparamters we choose in the cross validation part
+
+NN_test = Neural_Network(input_layer_size=68, hidden_layer_size=20,output_layer_size=2, Lambda=2e-6)  # initialize NN #input_layer_size=input_DNA.shape[0], output_layer_size=outputs.shape[0], hidden_layer_size=10, Lambda=0.00000005
+input_train, output_train, input_test, output_test=select_sequences('/Users/stephaniewankowicz/Dropbox/BMI_203/Final_Project/New_Neg_seq.fa','rap1-lieb-positives.txt', 1)
+# Use previously found validated parameters for training
+input_DNA_train=DNA_input(input_train)
+input_DNA_test=DNA_input(input_test)
+print(input_DNA_train)
+print(output_train.shape)
+NN_test.train(input_DNA_train, output_train, iterations=1000,learning_rate=0.5)
+#now we are going to test and output a test set.
+test_seq=[]
+test_file= open('rap1-lieb-test.txt', 'r')
+for line in test_file.readlines():
+    test_seq.append(line.strip())
+test_seq_input=DNA_input(test_seq)
+print(test_seq_input)
+print(test_seq_input.shape)
+test_output=NN_test.forward(test_seq_input)
+out = open("predictions.txt", "w")
+print('str')
+print(test_output.shape)
+print(test_output)
+for i in range(0, len(test_output)):
+    #print('test output 1')
+    #print(test_output[i,1])
+    test_output=NN_test.forward(test_seq_input[i])
+    print(test_output)
+    out.write(test_seq[i]+'\t'+str(test_output[i]))
+    out.write('\n')
+out.close()
