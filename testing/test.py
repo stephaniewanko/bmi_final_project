089ee6b60e6482404624e2e4c750591034b5b7af
Stephanie (Mullane) Wankowicz <15824327+stephaniewanko@users.noreply.github.com>
2019-03-15 23:12:03 -0700

    Update test.py

--- testing/test.py
+++ testing/test.py
@@ -4,11 +4,13 @@ sys.path.append('../scripts/')
 from scripts import NN
 
 #testing the autoencoder
-x = np.identity(8)
-y = np.identity(8)
-NN_auto = NN.Neural_Network(input_layer_size=8, hidden_layer_size=3, output_layer_size=8, Lambda=2e-6)
-NN_auto.train(x,y,10000,0.45)
-predict=NN_auto.forward(x)
-assert np.array_equal(x, predict.round())
-
-assert 1==1
+def test_auto():
+  x = np.identity(8)
+  y = np.identity(8)
+  NN_auto = NN.Neural_Network(input_layer_size=8, hidden_layer_size=3, output_layer_size=8, Lambda=2e-6)
+  NN_auto.train(x,y,10000,0.45)
+  predict=NN_auto.forward(x)
+  assert np.array_equal(x, predict.round())
+test_auto()
+def test_test():
+  assert 1==1
